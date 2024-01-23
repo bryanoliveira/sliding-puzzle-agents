2f7fd6be9a7b9f94a7a0da3723493a2952e7a9cd
Bryan Lincoln <bryanufg@gmail.com>
2024-01-23 10:27:56 -0300

    fix: update experiments

--- experiments/mnist.sh
+++ experiments/mnist.sh
@@ -8,16 +8,16 @@ python train.py --seed 420 --env-w 2 --env-h 2 --env-shuffle-steps 8 --env-varia
 python train.py --seed 650 --env-w 2 --env-h 2 --env-shuffle-steps 8 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
 
 # 3x3
-# python train.py --seed 304 --env-w 3 --env-h 3 --env-shuffle-steps 18 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
-# python train.py --seed 420 --env-w 3 --env-h 3 --env-shuffle-steps 18 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
-# python train.py --seed 650 --env-w 3 --env-h 3 --env-shuffle-steps 18 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+python train.py --seed 304 --env-w 3 --env-h 3 --env-shuffle-steps 18 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+python train.py --seed 420 --env-w 3 --env-h 3 --env-shuffle-steps 18 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+python train.py --seed 650 --env-w 3 --env-h 3 --env-shuffle-steps 18 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
 
-# # 4x4
-# python train.py --seed 304 --env-w 4 --env-h 4 --env-shuffle-steps 32 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
-# python train.py --seed 420 --env-w 4 --env-h 4 --env-shuffle-steps 32 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
-# python train.py --seed 650 --env-w 4 --env-h 4 --env-shuffle-steps 32 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+# 4x4
+python train.py --seed 304 --env-w 4 --env-h 4 --env-shuffle-steps 32 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+python train.py --seed 420 --env-w 4 --env-h 4 --env-shuffle-steps 32 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+python train.py --seed 650 --env-w 4 --env-h 4 --env-shuffle-steps 32 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
 
-# # 5x5
-# python train.py --seed 304 --env-w 5 --env-h 5 --env-shuffle-steps 50 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
-# python train.py --seed 420 --env-w 5 --env-h 5 --env-shuffle-steps 50 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
-# python train.py --seed 650 --env-w 5 --env-h 5 --env-shuffle-steps 50 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+# 5x5
+python train.py --seed 304 --env-w 5 --env-h 5 --env-shuffle-steps 50 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+python train.py --seed 420 --env-w 5 --env-h 5 --env-shuffle-steps 50 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
+python train.py --seed 650 --env-w 5 --env-h 5 --env-shuffle-steps 50 --env-variation image --env-image-folder ./imgs/mnist/trainingSet --policy CnnPolicy
