550b7256fbeaf0574f80bb5ebfa8c80d051f1c8e
Bryan Lincoln <bryanufg@gmail.com>
2024-01-24 19:41:47 -0300

    fix: update folders

--- env_test.py
+++ env_test.py
@@ -2,20 +2,28 @@ import sliding_puzzles
 
 env = sliding_puzzles.make(
     render_mode="human",
-    w=3,
-    h=3,
+    w=2,
+    h=2,
     sparse_rewards=True,
-    shuffle_steps=18,
-    variation="normalized",
-)
-obs = env.reset()
+    sparse_mode=sliding_puzzles.env.SparseMode.invalid_and_win,
+    shuffle_steps=50,
 
+    variation="normalized",
+
+    # variation="image",
+    # image_folder="./imgs/mnist/",
+)
+
+obs = env.reset()
+total_reward = 0
 while True:
     env.render()
     obs, reward, done, trunc, info = env.step(None)
+    total_reward += reward
     print(reward)
     if done or trunc:
-        print("Done!")
-        break
+        print("Done! ", total_reward)
+        total_reward = 0
+        obs = env.reset()
 
 env.close()
